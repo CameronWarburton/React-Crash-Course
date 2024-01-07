1be045f66c9daed2a751dab4fa7a0c4c1b4eca30
Cameron <cameron.cw@outlook.com>
2024-01-07 17:28:50 +0000

    Started Todo List

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>My Todo List</h1>
+    <div>
+      <h2>Finish Frontend Simplified</h2>
+      <button>Delete</button>
+    </div>
+    <div>
+      <h2>Finish The Interview Section</h2>
+      <button>Delete</button>
+    </div>
+    <div>
+      <h2>Land 100k Job</h2>
+      <button>Delete</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
