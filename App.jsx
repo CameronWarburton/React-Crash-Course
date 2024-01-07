15552171f6fb4ab82c6e438004ec4d694f9f2ee7
Cameron <cameron.cw@outlook.com>
2024-01-07 17:45:11 +0000

    Added Component to App file

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Todo from './components/Todo.jsx'
 
 function App() {
   return (
     <div>
     <h1>My Todo List</h1>
+    <Todo></Todo>
     <div>
       <h2>Finish Frontend Simplified</h2>
       <button>Delete</button>
