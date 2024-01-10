1e9ae14bb2a919b6da2d40b938ac2d2d95970747
Cameron <cameron.cw@outlook.com>
2024-01-10 16:28:18 +0000

    Added secondary props

--- App.jsx
+++ App.jsx
@@ -1,18 +1,27 @@
 import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx"
+import Modal from "./components/Modal.jsx";
 
 function App() {
   return (
     <div>
       <Title />
       <div className="todo__wrapper">
-        <Todo title="Finish Frontend Simplified"/>
-        <Todo title="Finish Interview Section"/>
-        <Todo title="Land a 100K Job"/>
+        <Todo
+          title="Finish Frontend Simplified"
+          paragraph="Code along with Frontend Simplified step by step"
+        />
+        <Todo
+          title="Finish Interview Section"
+          paragraph="Finish every interview question in the next 6 weeks."
+        />
+        <Todo 
+        title="Land a 100K Job" 
+        paragraph="Apply to 100 Jobs" 
+        />
       </div>
-      {/* <Modal /> */}
+      <Modal title="Are you sure?"/>
     </div>
   );
 }
