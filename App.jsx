20a9f306716d6d6b3928e89bc828b32cf6458e49
Cameron <cameron.cw@outlook.com>
2024-01-11 17:09:10 +0000

    Using React Hooks

--- App.jsx
+++ App.jsx
@@ -2,8 +2,11 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
+import React, { useState } from 'react';
 
 function App() {
+const [showModal, setShowModal] = useState(false)
+
   return (
     <div>
       <Title />
@@ -11,14 +14,14 @@ function App() {
         <input type="text" onChange={(event) => {
           console.log(event.target.value)
         }}/>
-        <button>Add todo</button>
+        <button onClick={() => setShowModal(true)}>Add todo</button>
       </div>
       <div className="todo__wrapper">
         <Todo title="Finish Frontend Simplified" />
         <Todo title="Finish Interview Section" />
         <Todo title="Land a 100K Job" />
       </div>
-      <Modal title="Are you sure you want to delete?"/>
+      {showModal && <Modal title="Confirm Delete?"/>}
     </div>
   );
 }
