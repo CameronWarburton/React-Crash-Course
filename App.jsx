abd1a7669d0190ff8a0a3887123b6e1e87fb44e0
Cameron <cameron.cw@outlook.com>
2024-01-11 14:36:23 +0000

    Adding event handlers

--- App.jsx
+++ App.jsx
@@ -7,21 +7,16 @@ function App() {
   return (
     <div>
       <Title />
-      <div className="todo__wrapper">
-        <Todo
-          title="Finish Frontend Simplified"
-          paragraph="Code along with Frontend Simplified step by step"
-        />
-        <Todo
-          title="Finish Interview Section"
-          paragraph="Finish every interview question in the next 6 weeks."
-        />
-        <Todo 
-        title="Land a 100K Job" 
-        paragraph="Apply to 100 Jobs" 
-        />
+      <div>
+        <input type="text" />
+        <button>Add todo</button>
       </div>
-      <Modal title="Are you sure?"/>
+      <div className="todo__wrapper">
+        <Todo title="Finish Frontend Simplified" />
+        <Todo title="Finish Interview Section" />
+        <Todo title="Land a 100K Job" />
+      </div>
+      {/* <Modal title="Are you sure you want to delete?"/> */}
     </div>
   );
 }
