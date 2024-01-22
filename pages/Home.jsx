df241be6380c51f2531542fa6eb5b6fdeca6f0c0
Cameron <cameron.cw@outlook.com>
2024-01-22 14:19:53 +0000

    Using API's and Loading State

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,5 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function Home() {
-    return <h1>Home</h1>
+  const [users, setUsers] = useState([]);
+
+  async function fecthUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+    console.log(data)
+  }
+
+  useEffect(() => {
+    setTimeout (() => {
+        fecthUsers();
+    }, 500);
+  }, []);
+
+  return (
+    <div>
+        {users.length > 0 
+        ?
+        <h1>{users[0]?.name}</h1>
+        : <h1>Loading...</h1>
+        }
+    </div>
+  );
 }
 
-export default Home
+export default Home;
