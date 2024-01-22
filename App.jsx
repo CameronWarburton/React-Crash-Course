7ec69d7f07a452f48a3fbfce23b696584f254c9a
Cameron <cameron.cw@outlook.com>
2024-01-22 12:51:26 +0000

    Added New Routes

--- App.jsx
+++ App.jsx
@@ -1,9 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Contact from "./pages/Contact.jsx";
 import Nav from "./components/Nav.jsx"
+import Users from "./pages/Users.jsx"
 
 function App() {
  
@@ -12,8 +11,7 @@ function App() {
        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/users/:username" element={<Users />} />
         </Routes>
       </Router>
   );
